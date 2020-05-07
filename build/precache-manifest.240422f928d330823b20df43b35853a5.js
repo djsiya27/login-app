3007da61e4e1c3b12ae5761cbf456507e2893866
siyabonga majola <siyabongamavundla@yahoo.com>
2020-05-07 11:23:49 +0200

    Build Commit

---
+++ build/precache-manifest.240422f928d330823b20df43b35853a5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2cae524db8c860eef09195a56754b57",
+    "url": "/login-app/index.html"
+  },
+  {
+    "revision": "e0a7eccab654ed5671ae",
+    "url": "/login-app/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "b7b7c86c98f89bbc4301",
+    "url": "/login-app/static/css/main.298aa595.chunk.css"
+  },
+  {
+    "revision": "e0a7eccab654ed5671ae",
+    "url": "/login-app/static/js/2.7789ee29.chunk.js"
+  },
+  {
+    "revision": "b7b7c86c98f89bbc4301",
+    "url": "/login-app/static/js/main.ee0d72e7.chunk.js"
+  },
+  {
+    "revision": "0d595c1f2f62de080ab0",
+    "url": "/login-app/static/js/runtime~main.8a02c311.js"
+  },
+  {
+    "revision": "c61e7d3abc988a9e8cdb814e0c851fef",
+    "url": "/login-app/static/media/pic1.c61e7d3a.jpg"
+  },
+  {
+    "revision": "7b0ab1cc80eacfc66ebb06ca5e057688",
+    "url": "/login-app/static/media/pic2.7b0ab1cc.jpg"
+  },
+  {
+    "revision": "b491156281fe72a9397d14ade143fd5d",
+    "url": "/login-app/static/media/pic3.b4911562.jpg"
+  }
+]);
